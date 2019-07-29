4d7e7454f9b3e9e8eb402db5df0d8dd09da4b2f9
qianqing <qian.qing@aliyun.com>
2019-07-29 15:14:40 +0800

    fix bug

--- src/libs/cloud-request.js
+++ src/libs/cloud-request.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import log from './cloud-log';
+const isProduction = process.env.NODE_ENV === 'production';
 
 export default async function (config, url, json, timeout = 25000) {
   if (!config || !url || !json) {
@@ -20,17 +21,21 @@ export default async function (config, url, json, timeout = 25000) {
     let data = res.data;
     if (!data) {
       log.e(baseURL, url, json, '云端未返回data', start, end);
-      return { IsSuccess: false, ErrorMsg: `后端无响应` };
+      return { IsSuccess: false, ErrorMsg: isProduction ? '哦嚯～网络开小差了，请稍后重新打开试试' : '云端无响应' };
     }
     if (data.HasError) {
       log.e(baseURL, url, json, data.Fault.ErrorDescription, start, end);
-      return { IsSuccess: false, ErrorMsg: `${data.Fault.ErrorDescription}` };
+      if (isProduction && data.Fault.ErrorDescription.indexOf('Could not connect to net.tcp') !== -1 && data.Fault.ErrorDescription.indexOf('Server Application Unavailable') !== -1) {
+        return { IsSuccess: false, ErrorMsg: '哦嚯～网络开小差了，请稍后重新打开试试'};
+      } else {
+        return { IsSuccess: false, ErrorMsg: `${data.Fault.ErrorDescription}` };
+      }
     }
     log.i(baseURL, url, json, data, start, end);
-    return {IsSuccess: true, Data: data.Body, Paging: data.Paging};
+    return { IsSuccess: true, Data: data.Body, Paging: data.Paging };
   } catch (error) {
     let end = new Date();
     log.e(baseURL, url, json, error.message, start, end);
-    return { IsSuccess: false, ErrorMsg: `${error.message}` };
+    return { IsSuccess: false, ErrorMsg: isProduction ? '哦嚯～网络开小差了，请稍后重新打开试试' : `${error.message}` };
   }
 }
