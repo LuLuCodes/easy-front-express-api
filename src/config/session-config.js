41b52b5d2c3be21bf5e3bda4d9c63132e917237c
mac <mac@macdeMac-7.local>
2020-09-19 10:56:02 +0800

    fix bug

--- src/config/session-config.js
+++ src/config/session-config.js
@@ -2,8 +2,8 @@ export default {
   cookie: { secure: false, maxAge: 3 * 24 * 3600 * 1000, httpOnly: true },
   redis: {
     host: process.env.REDIS_HOST || '127.0.0.1',
-    port: process.env.REDIS_PORT || 6379,
-    db: process.env.COOKIE_REDIS_DB_INDEX || 0,
+    port: parseInt(process.env.REDIS_PORT) || 6379,
+    db: parseInt(process.env.COOKIE_REDIS_DB_INDEX) || 0,
   },
   secret: process.env.COOKIE_SECRET || 'express-front-express-api',
   key: process.env.COOKIE_KEY || 'express-front-express-api',
